450460ac57e6c426e6f97888a5babe3838548242
Abhra01 <abhradeepdebnath@gmail.com>
2024-08-10 09:53:18 -0400

    reinitialize

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-400">
+      hey
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ShoppingCart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShoppingCart() {
+  return (
+    <div className='w-44 h-fit'>
+        
+    </div>
+  )
+}
+
+export default ShoppingCart;
